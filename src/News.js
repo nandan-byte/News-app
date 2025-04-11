0dc12868a7aaa7c3b1e516d66534ca31eac324e6
Nandan B Deshamukh <158804961+nandan-byte@users.noreply.github.com>
2025-04-11 17:14:36 +0530

    Update News.js

--- src/News.js
+++ src/News.js
@@ -1,27 +1,29 @@
-export default function News(props)
-{
-    
+export default function News(props) {
     return (
         <div className="news">
             <div className="news-img">
                 {
                     props.article.urlToImage !== null ?
-                    <img src={props.article.urlToImage}/>
-                    : <img src="https://th.bing.com/th/id/OIP.wDo8BJzyoOT-_7vRvgGrlgHaH0?pid=ImgDet&w=185&h=195&c=7&dpr=1.3"/>
+                    <img 
+                        src={props.article.urlToImage} 
+                        alt={props.article.title || "News image"} 
+                    />
+                    : 
+                    <img 
+                        src="https://th.bing.com/th/id/OIP.wDo8BJzyoOT-_7vRvgGrlgHaH0?pid=ImgDet&w=185&h=195&c=7&dpr=1.3" 
+                        alt="Default news image" 
+                    />
                 }
-               
             </div>
+
             <h1>{props.article.title}</h1>
 
-            <p>{props.article.description?.substring(0,100).concat("...")} <a href={props.article.url} target="_blank">Read more</a></p>
-
-           
+            <p>{props.article.description?.substring(0,100).concat("...")} <a href={props.article.url} target="_blank" rel="noopener noreferrer">Read more</a></p>
 
             <div className="source">
                 <p>{props.article.author}</p>
                 <p>{props.article.source.name}</p>
             </div>
-
         </div>
-    )
+    );
 }
